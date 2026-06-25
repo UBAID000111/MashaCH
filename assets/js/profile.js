b439037ef02ea71f06bcffbd9e50fb746a150a26
Mohd Ubaid <ubaidji07@gmail.com>
2026-06-25 08:26:58 +0530

    Initial commit

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,1 +1,57 @@
-console.log("Profile Loaded");
+import { auth, db } from "../firebase/firebase-config.js";
+
+import {
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+        window.location.href = "login.html";
+        return;
+    }
+
+    try {
+
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+
+            const data = docSnap.data();
+
+            userName.textContent = data.name;
+            userEmail.textContent = data.email;
+
+        } else {
+
+            userName.textContent = "User";
+            userEmail.textContent = user.email;
+
+        }
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+});
+
+logoutBtn.addEventListener("click", async () => {
+
+    await signOut(auth);
+
+    window.location.href = "login.html";
+
+});
