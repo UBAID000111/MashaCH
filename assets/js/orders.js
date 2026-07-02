b68a252b663b3e393bbed2cbeeefc71c8fa1358d
Mohd Ubaid <ubaidji07@gmail.com>
2026-07-02 22:37:22 +0530

    Initial commit

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -1,1 +1,54 @@
-console.log("Orders Loaded");
+import { auth, db } from "../firebase/firebase-config.js";
+
+import {
+collection,
+query,
+where,
+getDocs,
+orderBy
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+import {
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+optimizeImage
+} from "./services/imageService.js";
+
+/*=============================
+DOM
+=============================*/
+
+const loading =
+document.getElementById("ordersLoading");
+
+const empty =
+document.getElementById("emptyOrders");
+
+const container =
+document.getElementById("ordersContainer");
+
+const search =
+document.getElementById("searchOrder");
+
+let allOrders=[];
+
+/*=============================
+AUTH
+=============================*/
+
+onAuthStateChanged(auth,async(user)=>{
+
+if(!user){
+
+location.href="login.html";
+
+return;
+
+}
+
+await loadOrders(user.uid);
+
+});
+
