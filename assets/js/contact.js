e0f51fff543ed5202805a87bb97df17560268c31
Mohd Ubaid <ubaidji07@gmail.com>
2026-07-04 03:31:27 +0530

    Initial commit

---
+++ assets/js/contact.js
@@ -0,0 +1,40 @@
+const btn = document.getElementById("sendBtn");
+
+btn.onclick = () => {
+
+const name =
+document.getElementById("name").value.trim();
+
+const phone =
+document.getElementById("phone").value.trim();
+
+const message =
+document.getElementById("message").value.trim();
+
+if(!name || !phone || !message){
+
+alert("Please fill all fields.");
+
+return;
+
+}
+
+const text =
+
+`*Masha Wear Contact Request*%0A%0A`+
+
+`👤 Name: ${name}%0A`+
+
+`📞 Phone: ${phone}%0A%0A`+
+
+`📝 Issue:%0A${message}`;
+
+window.open(
+
+`https://wa.me/919999950919?text=${text}`,
+
+"_blank"
+
+);
+
+};
