6795805197ea94d4b68d02a4ac82fb9cf500b132
Mohd Ubaid <ubaidji07@gmail.com>
2026-07-19 19:41:07 +0530

    Initial Commit

--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,51 @@
+
+
+
 const { onCall, HttpsError } = require("firebase-functions/v2/https");
+const { defineSecret } = require("firebase-functions/params");
 const admin = require("firebase-admin");
 const Razorpay = require("razorpay");
 
 admin.initializeApp();
 
-const razorpay = new Razorpay({
-    key_id: "rzp_live_T8gJhOFIVYCxkz",
-    key_secret: "gk33923fxwep3XcBMNO2oy3v"
-});
+const razorpayKeyId = defineSecret("RAZORPAY_KEY_ID");
+const razorpayKeySecret = defineSecret("RAZORPAY_KEY_SECRET");
 
-exports.createOrder = onCall(async (request) => {
-
+exports.createOrder = onCall(
+  {
+    secrets: [razorpayKeyId, razorpayKeySecret],
+  },
+  async (request) => {
     try {
+      const razorpay = new Razorpay({
+        key_id: razorpayKeyId.value(),
+        key_secret: razorpayKeySecret.value(),
+      });
 
-        const { amount, receipt } = request.data;
+      const { amount, receipt } = request.data;
 
-        if (!amount) {
-            throw new HttpsError(
-                "invalid-argument",
-                "Amount is required"
-            );
-        }
+      if (!amount) {
+        throw new HttpsError(
+          "invalid-argument",
+          "Amount is required"
+        );
+      }
 
-        const order = await razorpay.orders.create({
-            amount: amount * 100,
-            currency: "INR",
-            receipt: receipt || ("MCH_" + Date.now())
-        });
+      const order = await razorpay.orders.create({
+        amount: amount * 100,
+        currency: "INR",
+        receipt: receipt || ("MCH_" + Date.now()),
+      });
 
-        return {
-            order
-        };
+      return { order };
 
     } catch (err) {
+      console.error(err);
 
-        console.error(err);
-
-        throw new HttpsError(
-            "internal",
-            err.message
-        );
-
+      throw new HttpsError(
+        "internal",
+        err.message
+      );
     }
-
-});
+  }
+);
